b46eec1ee8d1548289b271058ac788d631b8ba5e
David Podhola <david@podhola.net>
2019-05-16 14:35:50 +0200

    code docs

--- src/main/kotlin/org/compiere/product/ProductPO.kt
+++
@@ -1,8 +0,0 @@
-package org.compiere.product
-
-import kotliquery.Row
-import org.compiere.orm.PO
-import software.hsharp.core.util.Environment
-import software.hsharp.modules.Module
-
-abstract class ProductPO(row: Row?, id: Int) : PO(row, id), Module by Environment<Module>().module
--- src/main/kotlin/org/compiere/product/ProductPOUser.kt
+++ src/main/kotlin/org/compiere/product/ProductPOUser.kt
@@ -5,4 +5,7 @@ import org.compiere.orm.BasePOUser
 import software.hsharp.core.util.Environment
 import software.hsharp.modules.Module
 
+/**
+ * Persistent object in Product module with the userId
+ */
 abstract class ProductPOUser(row: Row?, id: Int) : BasePOUser(row, id), Module by Environment<Module>().module
